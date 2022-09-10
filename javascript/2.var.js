1e7f65ea989c1d926852f7490aba2b139b148f50
DEV-RAPHAEL <uniqueinternet2020@gmail.com>
2022-09-09 19:17:35 -0700

    E sure die, we know variables

--- javascript/2.var.js
+++ javascript/2.var.js
@@ -9,8 +9,8 @@
 //Underscores
 //case senstives
 
-name = 'Hannah'
-Name = 'Nnamdi'
+let name = 'Hannah'
+let Name = 'Nnamdi'
 
 console.log(name)
 console.log(Name)
@@ -19,14 +19,28 @@ console.log(Name)
 let Length = 60;
 let Breadth = 20;
 
-Area = console.log("Area of this Rectangle is " +  60 * 20)
+let Area = console.log("Area of this Rectangle is " +  60 * 20)
 Area
 
 /* Simple Interest Calculator */
-P = 3000;
-R = 3;
-T = 5;
+let P = 3000;
+let R = 3;
+let T = 5;
 
-SI = P*R*T/100
+let SI = P*R*T/100
 
 console.log("The Simple Interest is " + SI)
+
+/* Mini Composition */
+let firstName = 'Nnamdi' 
+let age = 16
+let job = "Jobless"
+let hobby = "Playing Instrument"
+let hobby2 = "Gaming"
+let hobby3 = "Coding"
+let Rel = "Complicated"
+
+let Composition = "My name is " + firstName + " I am " + age + " Years Old" + " I am looking for Job, hence, I am " +
+job + " My Hobbies are "  + hobby + " " + hobby2 + " " + hobby3 + " " + " I am in a " + Rel + " Relationship"
+
+console.log(Composition)
