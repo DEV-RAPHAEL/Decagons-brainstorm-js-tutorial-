108b2eb5f171eaa25ced5614af7b9ffe3f5a3907
DEV-RAPHAEL <uniqueinternet2020@gmail.com>
2022-09-09 19:28:29 -0700

    Data types sha

---
+++ javascript/3.dataTypes.js
@@ -0,0 +1,9 @@
+let first = "String";
+let second = 1;
+let third = true;
+let fourth = null;
+
+console.log(typeof(first))
+console.log(typeof(second))
+console.log(typeof(third))
+console.log(typeof(fourth))
