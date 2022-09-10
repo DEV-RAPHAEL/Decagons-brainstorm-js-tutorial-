c057c65602be1ce7a86708042a3b2bf6383c3e40
DEV-RAPHAEL <uniqueinternet2020@gmail.com>
2022-09-09 20:04:52 -0700

    Array.push(Decadevs in the building)

---
+++ javascript/5.array.js
@@ -0,0 +1,14 @@
+const decadevs = ["Hannah","Nnamdi", "Timothy", "Matthew", "Raphael"]
+console.log(decadevs)
+decadevs.pop("Raphael")
+console.log(decadevs)
+decadevs.push("Anybody")
+console.log(decadevs)
+decadevs.sort()
+console.log(decadevs)
+
+const print = console.log
+const nameAge = ["Raph", "Jane", 9, "Foo", 19, "Bas", 101]
+print(nameAge)
+nameAge.sort()
+print(nameAge)
