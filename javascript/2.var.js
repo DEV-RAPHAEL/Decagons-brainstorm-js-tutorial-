763c5f8bb572ea67f95069857cf4990fee91d854
DEV-RAPHAEL <uniqueinternet2020@gmail.com>
2022-09-09 19:00:30 -0700

    Simple Interest && Area of Rectangle

---
+++ javascript/2.var.js
@@ -0,0 +1,32 @@
+//RULES
+//1. Reserved keywords
+//let let = 1;
+//console.log(let)
+//2. Variable names can contain the following:
+//Letters
+//Numbers
+//Dollar sign
+//Underscores
+//case senstives
+
+name = 'Hannah'
+Name = 'Nnamdi'
+
+console.log(name)
+console.log(Name)
+
+/* Area of Rectangle */
+let Length = 60;
+let Breadth = 20;
+
+Area = console.log("Area of this Rectangle is " +  60 * 20)
+Area
+
+/* Simple Interest Calculator */
+P = 3000;
+R = 3;
+T = 5;
+
+SI = P*R*T/100
+
+console.log("The Simple Interest is " + SI)
